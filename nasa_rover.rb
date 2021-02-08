47bf69b268fae1f37bb3424f1809b0ac9f9c3b59
Marcela Brenes Daniels <marce89455@gmail.com>
2021-02-08 15:51:31 -0600

    Minor issue fixed: width, information running smoothly still

--- nasa_rover.rb
+++ nasa_rover.rb
@@ -2,8 +2,6 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-
-
 def request (url,token = nil)
     url = URI("#{url}?sol=40&api_key=#{token}")
     https = Net::HTTP.new(url.host, url.port)
@@ -16,20 +14,22 @@ end
 #Index HTML
 def build_web_page(data_hash)
     File.open('nasa_index.html', 'w') do |file|
-        photos = data_hash["photos"]
         file.puts"<html>"
         file.puts"<head>"
         file.puts"<title>Mars Curiosity Rover</title>"
         file.puts"</head>"
-        file.puts"<body>"
+        file.puts"<body style='text-align:center'>"
         file.puts"<h1>Mars Rover Photos</h1>"
-        file.puts"<p>The following pictures where taken by Nasa's Mars Curiosity Rover an intelligent robot with a mission:</p>"
-        file.puts"<h2>Discovering if the planet can contain small forms of life!</h2>"
+        file.puts"<h2>The following pictures where taken by Nasa's Mars Curiosity Rover, an intelligent robot with a mission:</h2>"
+        file.puts"<h3>Discovering if the red planet can contain small forms of life!</h3>"
+        file.puts"<ul>"
+        photos = data_hash["photos"]
         photos.each do |photo|
-        file.puts"<img src='#{photo["img_src"]}'/>"
-        file.puts"<h3>'Camera: #{photo["camera"]["name"]}'</h3>"
-        file.puts"<h3>'Id: #{photo["camera"]["id"]}'</h3>"
+            file.puts"<li><img src='#{photo["img_src"]}' width='300'/></li>"
+            file.puts"<h3>Id: #{photo["id"]}</h3>"
+            file.puts"<h3>Camera: #{photo["camera"]["name"]}</h3>"
         end
+        file.puts"</ul>"
         file.puts"</body>"
         file.puts"</html>"
     end
